e0e796de35667b7f714391bd8a17e3dcc87acc7c
zhiyuuuu <liuzy0815@gmail.com>
2023-03-25 16:18:39 +0800

    login basic structure

--- frontend/src/Containers/Login.js
+++ frontend/src/Containers/Login.js
@@ -1,3 +1,19 @@
+import { Button } from 'antd';
+import '../css/Login.css';
+
 const Login = () => {
-    
+    return(
+        <div className="login-container">
+            <div className="block">
+                <div className="login-description">
+                    <p>Login with your GitHub account</p>
+                </div>
+                <div className="login-button">
+                    <Button type='primary'> Log in </Button>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default Login;
