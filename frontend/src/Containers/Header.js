34a32fb86684173bb667b60582f496a5a5590c1c
zhiyuuuu <liuzy0815@gmail.com>
2023-04-01 11:31:44 +0800

    init header

---
+++ frontend/src/Containers/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return(
+        <div className="header-container">
+            <div className="filter-time"></div>
+            <div className="filter-state"></div>
+            <div className="searchbar"></div>
+            <div className="logout"></div>
+        </div>
+    )
+}
+
+export default Header;
