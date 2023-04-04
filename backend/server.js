27e8865650eea2986e6d725e8117b02da5b5a726
zhiyuuuu <liuzy0815@gmail.com>
2023-04-04 15:32:28 +0800

    update server

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(cors());
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Credentials', 'true')
